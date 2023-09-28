fa2570077852951061288a6ed366191d3a8d32d7
zezoexx <136598961+zezoexx@users.noreply.github.com>
2023-09-28 23:32:05 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,5 @@
-// JavaScript Document
-console.log("hi");
-
 $(document).ready(function(){
-	$('#nav-icon3').click(function(){
+	$('#hamburger').click(function(){
 		$(this).toggleClass('open');
 	});
 });
@@ -18,25 +15,7 @@ function toggleMenu() {
 }
 
 
-functionElement.addEventListener('click', function() {
-	if (deNav.classList.contains('open')) {
-	  firstThreeListItems.classList.add('hide');
-	}
-  });
 
   
-  var menuButton = document.querySelector("header button");
-  var deNav = document.querySelector("header nav:first-of-type");
-  const firstUl = document.querySelector('header nav ul:first-child');
-  const firstThreeListItems = firstUl.querySelectorAll('li:nth-child(-n+3)');
   
-  menuButton.addEventListener('click', toggleMenu);
-  
-  function toggleMenu() {
-	deNav.classList.toggle("open");
-	if (deNav.classList.contains("open")) {
-		firstThreeListItems.classList.add('hide');
-	
-	}
-  }
   
